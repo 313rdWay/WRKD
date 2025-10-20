cbc3a636c473abce2c7af81d086bd9a8589ffd8c
Davaughn Williams <65515720+313rdWay@users.noreply.github.com>
2025-10-20 15:27:35 -0400

    Initial Commit

---
+++ WRKD/WRKDApp.swift
@@ -0,0 +1,17 @@
+//
+//  WRKDApp.swift
+//  WRKD
+//
+//  Created by Davaughn Williams on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WRKDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WRKDTests/WRKDTests.swift
@@ -0,0 +1,17 @@
+//
+//  WRKDTests.swift
+//  WRKDTests
+//
+//  Created by Davaughn Williams on 10/20/25.
+//
+
+import Testing
+@testable import WRKD
+
+struct WRKDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
